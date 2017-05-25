baa07fe89e52afa7a464a30623e2247ff1857ed0
Hans Phung <hanhpv@dtn.com.vn>
2017-05-25 12:44:51 +0700

    allow to pass the PFM user and group ID through environment variables to fix file permission issue

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,7 +11,17 @@ function fixFilesystemPermissions() {
 # Replace Xdebug remote host with ENV variable
 sed -i "s|;*xdebug.remote_host=.*|xdebug.remote_host=${XDEBUG_REMOTE_HOST}|i" /etc/php7/conf.d/xdebug.ini
 
-fixFilesystemPermissions 
+# fixFilesystemPermissions
+
+# create a new user and run php-fpm under this user
+FPM_USER='magento'
+FPM_GROUP='magento'
+if [ ! -z "${FPM_UID}" ] && [ ! -z "${FPM_GID}" ]; then
+    addgroup -g ${FPM_GID} ${FPM_GROUP}
+    adduser -u ${FPM_UID} -G ${FPM_GROUP} -g 'Linux User named' -s /sbin/nologin -D ${FPM_USER}
+    sed -i "s|;*user = nobody|user = ${FPM_USER}|i" /etc/php7/php-fpm.d/www.conf
+    sed -i "s|;*group = nobody|group = ${FPM_GROUP}|i" /etc/php7/php-fpm.d/www.conf
+fi
 
 # Start PHP-FPM service
 exec "/usr/sbin/php-fpm7"
